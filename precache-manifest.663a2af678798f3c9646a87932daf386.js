47f6f2d3c68b082a0a0fc0ffa3718825bb8bec79
Branden LaCour <38292736+BrandenLaCour@users.noreply.github.com>
2019-01-10 15:12:13 -0600

    Updates

---
+++ precache-manifest.663a2af678798f3c9646a87932daf386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bad11645d2e7f5b9b4f5",
+    "url": "/face-finder/static/js/runtime~main.bad11645.js"
+  },
+  {
+    "revision": "ec187b85a69bb3f7a041",
+    "url": "/face-finder/static/js/main.ec187b85.chunk.js"
+  },
+  {
+    "revision": "da72a2a98b9a699b9726",
+    "url": "/face-finder/static/js/1.da72a2a9.chunk.js"
+  },
+  {
+    "revision": "ec187b85a69bb3f7a041",
+    "url": "/face-finder/static/css/main.0f2661ae.chunk.css"
+  },
+  {
+    "revision": "da72a2a98b9a699b9726",
+    "url": "/face-finder/static/css/1.6d3bf311.chunk.css"
+  },
+  {
+    "revision": "5f758714a7fc6dfb551ab686e5021f4c",
+    "url": "/face-finder/index.html"
+  }
+];
